1eaef8c80db8707a321b6a465e767849d5e5a0d1
juliaCottage <2426364+juliaCottage@users.noreply.github.com>
2023-03-23 11:02:34 -0400

    0.1.1

---
+++ inc/plugin-updates.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/juliaCottage/regenexx-outcomes',
+	__FILE__,
+	'regenexx-outcomes'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('your-token-here');
