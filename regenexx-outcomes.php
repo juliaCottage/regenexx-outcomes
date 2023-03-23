6ad611a71122b813d3cb5cd23c06bf29162d81b2
juliaCottage <2426364+juliaCottage@users.noreply.github.com>
2023-03-23 14:40:11 -0400

    0.1.7

--- regenexx-outcomes.php
+++ regenexx-outcomes.php
@@ -4,7 +4,7 @@
  * Description:       Regenexx Outcome Data for Provider Sites
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.6
+ * Version:           0.1.7
  * Author:            Klein New Media
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -14,7 +14,7 @@
  */
 
 
-define('ROP_VERSION', '0.1.6');
+define('ROP_VERSION', '0.1.7');
 
 include_once 'partials/setup.php';
 
