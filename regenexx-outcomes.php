c7d35007638eb821b5bc19abac8e05a3bc27adf9
juliaCottage <2426364+juliaCottage@users.noreply.github.com>
2023-09-06 08:17:43 -0400

    0.2.5

--- regenexx-outcomes.php
+++ regenexx-outcomes.php
@@ -4,7 +4,7 @@
  * Description:       Regenexx Outcome Data for Regenexx Provider Sites. Use shortcode [regenexx_outcomes].
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.2.3
+ * Version:           0.2.5
  * Author:            Klein New Media
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -14,7 +14,7 @@
  */
 
 // KNM5bad45a491b6c
-define('ROP_VERSION', '0.2.3');
+define('ROP_VERSION', '0.2.5');
 
 include_once 'partials/setup.php';
 
@@ -32,18 +32,18 @@ function targetdna_license_settings_link( $links ) {
 }
 
 
-if ($slm_status != 'active' || "" == get_option('license_key') ) {
+// if ($slm_status != 'active' || "" == get_option('license_key') ) {
 
-function sample_admin_notice__error() {
-	$class = 'notice notice-error';
-	$message = '<strong>Regenexx Outcomes Plugin:</strong> <a href="/wp-admin/options-general.php?page=knm-license">License key required.</a> Please contact <a href="email:support@kleinnewmedia.com">support@kleinnewmedia.com</a> to request a license.';
+// function sample_admin_notice__error() {
+// 	$class = 'notice notice-error';
+// 	$message = '<strong>Regenexx Outcomes Plugin:</strong> <a href="/wp-admin/options-general.php?page=knm-license">License key required.</a> Please contact <a href="email:support@kleinnewmedia.com">support@kleinnewmedia.com</a> to request a license.';
 
-	printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), $message  );
-}
-add_action( 'admin_notices', 'sample_admin_notice__error' );
+// 	printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), $message  );
+// }
+// add_action( 'admin_notices', 'sample_admin_notice__error' );
 
-  return;
-} else {
+//   return;
+// } else {
 
 // Autoload files in /inc/ directory
   foreach (glob(__DIR__ . '/inc/*.php') as $my_theme_filename) {
@@ -54,6 +54,6 @@ add_action( 'admin_notices', 'sample_admin_notice__error' );
     }
   }
 
-}
+// }
 
 
